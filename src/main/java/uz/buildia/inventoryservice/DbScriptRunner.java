be2fe992577907cf4715f60579afd29f955d1022
Javokhir-tech <abdullaev.dzhavokhir@mail.ru>
2024-12-09 21:00:55 +0500

    fix scripts

--- src/main/java/uz/buildia/inventoryservice/DbScriptRunner.java
+++ src/main/java/uz/buildia/inventoryservice/DbScriptRunner.java
@@ -17,12 +17,12 @@ public class DbScriptRunner implements CommandLineRunner {
     @Override
     @Transactional
     public void run(String... args) throws Exception {
-        entityManager.createNativeQuery("INSERT INTO inventory_service.scanners (hr_id) VALUES ('SC_001')").executeUpdate();
-        entityManager.createNativeQuery("INSERT INTO inventory_service.scanners (hr_id) VALUES ('SC_002')").executeUpdate();
+        entityManager.createNativeQuery("INSERT INTO inventory_service.scanners (hr_id) VALUES ('SC_001') ON CONFLICT DO NOTHING").executeUpdate();
+        entityManager.createNativeQuery("INSERT INTO inventory_service.scanners (hr_id) VALUES ('SC_002') ON CONFLICT DO NOTHING").executeUpdate();
 
-        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (1, 'model_of_product', 'first')").executeUpdate();
-        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (2, 'model_of_product', 'second')").executeUpdate();
-        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (3, 'model_of_product', 'third')").executeUpdate();
+        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (1, 'model_of_product', 'first') ON CONFLICT DO NOTHING").executeUpdate();
+        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (2, 'model_of_product', 'second') ON CONFLICT DO NOTHING").executeUpdate();
+        entityManager.createNativeQuery("INSERT INTO inventory_service.stages (id, model, stage_name) VALUES (3, 'model_of_product', 'third') ON CONFLICT DO NOTHING").executeUpdate();
 
         entityManager.createNativeQuery("UPDATE inventory_service.stages SET next_stage = 2 WHERE id = 1").executeUpdate();
         entityManager.createNativeQuery("UPDATE inventory_service.stages SET previous_stage = 1, next_stage = 3 WHERE id = 2").executeUpdate();
